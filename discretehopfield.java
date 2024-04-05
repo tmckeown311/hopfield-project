f0ac2ac23f574c4a877e3ce8e9a9b9e7f547421d
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2024-04-04 19:38:21 -0700

    working on file reading for setting weights

--- discretehopfield.java
+++ discretehopfield.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.Scanner;
 import java.util.random.*;
 
 public class discretehopfield {
@@ -9,6 +11,37 @@ public class discretehopfield {
         for (int i = 0; i < len; i++){
             neurons[i] = new neuron(numInputs, i);
         }
+    }
+    public void readWeightFile(File input){
+        Scanner weightScan;
+        try{
+            weightScan = new Scanner(input);
+            //jump past the headers of the file
+            weightScan.nextInt();
+            weightScan.nextInt();
+            while (weightScan.hasNext()) {
+                int[][] cur_matrix = new int[10][10];
+                for (int r = 0; r < 10; r++){
+                    for (int c = 0; c<10; c++){
+                        String cur_in = weightScan.next();
+                        if (cur_in == "0"){
+                            
+                        }
+                        else{
+
+                        }
+
+                    }
+
+                }
+                }
+
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+
     }
 
 }
